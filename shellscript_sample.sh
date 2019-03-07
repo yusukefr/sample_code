e298ec7a9ab8225ec0b887ca6891f98e14341e87
yusuke furuta <yusukefulltype@gmail.com>
2019-03-08 03:53:59 +0900

    項目の並びを変更

--- shellscript_sample.sh
+++ shellscript_sample.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-# システム関連---------------------------------------
-# シャットダウン・再起動
-sudo shutdown # 管理者権限必要
-poweroff
-reboot # rebootは内部でshutdown -r -q nowを呼び出しているだけ
-
 # ファイル表示---------------------------------------
 # 先頭25行の表示
 cat sample.txt | head -n 25
 # ログファイルの監視
 tail -f /var/log/messages
 
-# スクリプト自身のパスを取得---------------------------------------
-# パスに自身のパスを追加（%~dp0 で自身のパスを取得できる）
-set "PATH=%~dp0;%PATH%"
+# 文字コード---------------------------------------
+# カレントディレクトリ内の全ファイルの文字コードを一括でutf8に置換(上書き)
+find . -type f -print0 | xargs -0 nkf --overwrite -w -Lu
+# 文字コード&改行コードの判定
+nkf --guess *.md
+
+# 検索関連---------------------------------------
+# grep で単語単位で検索する
+dmesg | grep -w "video"
+# 検索結果にヒットした行の前後の行も表示
+cat sample.txt | grep -C 2 "video" # 前後2行も表示
+cat sample.txt | grep -A 2 "video" # 後2行も表示
+cat sample.txt | grep -B 2 "video" # 前2行も表示
+# ○○を含まない行だけを表示する
+ls -l /etc | grep -v "root"
+ls -l /proc/ | grep -v -E "root\s+root" # 正規表現使用
+# or検索
+cat sample.txt | grep -E "文字列1|文字列2|文字列3"
+# and検索
+cat sample.txt | grep "文字列1" | grep "文字列2"
 
 # 圧縮/解凍---------------------------------------
 # Windows環境で作成されたzipファイルを解凍(文字コードsjisを指定して解凍)
@@ -27,17 +38,17 @@ tar -czvf data0809.tgz * # gzip形式で圧縮
 # アーカイブファイル「data0809.tgz」を展開する
 tar -xzvf data0809.tgz
 
-# 文字コード---------------------------------------
-# カレントディレクトリ内の全ファイルの文字コードを一括でutf8に置換(上書き)
-find . -type f -print0 | xargs -0 nkf --overwrite -w -Lu
-# 文字コード&改行コードの判定
-nkf --guess *.md
+# パッケージ関連---------------------------------------
+# パッケージ管理にレポジトリを追加
+sudo apt-add-repository ppa:fish-shell/release-3
+# パッケージ管理からレポジトリを削除
+sudo add-apt-repository --remove ppa:fish-shell/release-2
 
-# CLI/GUIモード---------------------------------------
-sudo systemctl isolate multi-user.target # CLIモードにする
-sudo systemctl isolate graphical.target # GUIモードにする
-sudo systemctl set-default multi-user.target # デフォルトをCLIモードにする
-sudo systemctl set-default graphical.target # デフォルトをGUIモードにする
+# システム関連---------------------------------------
+# シャットダウン・再起動
+sudo shutdown # 管理者権限必要
+poweroff
+reboot # rebootは内部でshutdown -r -q nowを呼び出しているだけ
 
 # プロセス関連---------------------------------------
 # 実行中のプロセスを表示
@@ -54,21 +65,6 @@ pstree -U
 
 # プロセス停止
 kill <process number>
-
-# 検索関連---------------------------------------
-# grep で単語単位で検索する
-dmesg | grep -w "video"
-# 検索結果にヒットした行の前後の行も表示
-cat sample.txt | grep -C 2 "video" # 前後2行も表示
-cat sample.txt | grep -A 2 "video" # 後2行も表示
-cat sample.txt | grep -B 2 "video" # 前2行も表示
-# ○○を含まない行だけを表示する
-ls -l /etc | grep -v "root"
-ls -l /proc/ | grep -v -E "root\s+root" # 正規表現使用
-# or検索
-cat sample.txt | grep -E "文字列1|文字列2|文字列3"
-# and検索
-cat sample.txt | grep "文字列1" | grep "文字列2"
 
 # パーミッション---------------------------------------
 # 実行権限の付与
@@ -90,13 +86,17 @@ sudo chown web.api sample.sh
 ln -s ./app/log/ log # ディレクトリのシンボリックリンクを作成する
 ln -s ./bin/commandA commandA # ファイルのシンボリックリンクを作成する
 
+# CLI/GUIモード---------------------------------------
+sudo systemctl isolate multi-user.target # CLIモードにする
+sudo systemctl isolate graphical.target # GUIモードにする
+sudo systemctl set-default multi-user.target # デフォルトをCLIモードにする
+sudo systemctl set-default graphical.target # デフォルトをGUIモードにする
+
 # シェル---------------------------------------
 # デフォルトシェルをfishに変更
 # https://github.com/fish-shell/fish-shell/
 sudo chsh -s /usr/local/bin/fish
 
-# パッケージ関連---------------------------------------
-# パッケージ管理にレポジトリを追加
-sudo apt-add-repository ppa:fish-shell/release-3
-# パッケージ管理からレポジトリを削除
-sudo add-apt-repository --remove ppa:fish-shell/release-2
+# シェルスクリプト関連---------------------------------------
+# パスにスクリプト自身のパスを追加（%~dp0 で自身のパスを取得できる）
+set "PATH=%~dp0;%PATH%"
